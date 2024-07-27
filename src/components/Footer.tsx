1b42096b38988b004c009c08184ad5fdf95ed441
Aidan Froggatt <aidanfr@live.ca>
2024-07-27 18:46:22 -0400

    quick mockup of entire landing page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,35 @@
+
+
+const Footer = () => {
+    return (
+        <div id="footer" className="flex flex-col w-full py-32 gap-y-8">
+            <h3>SwingersGolf</h3>
+            <div className="flex flex-row justify-between">
+                <div id="contact-info">
+                    <p>1234 Address Road, City, Province</p>
+                    <p>123-456-7890</p>
+                    <div id="social-media-container" className="flex flex-row">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+                <div id="website-routes" className="flex flex-col gap-y-2">
+                    <p>Pages</p>
+                    <a href="/about">About</a>
+                    <a href="/contact">Contact</a>
+                    <a href="/privacy">Privacy</a>
+                    <a href="/terms">Terms</a>
+                </div>
+            </div>
+            <div id="legal-info" className="flex flex-row border-t border-t-black py-2 gap-x-4">
+                <p className="flex flex-grow">© 2024 SwingersGolf. All rights reserved.</p>
+                <a href="/privacy">Privacy Policy</a>
+                <a href="/terms">Terms of Service</a>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,8 +3,10 @@ import LandingPageBackground from "../assets/LandingPageBackground.svg";
 import Caption from "../components/Caption";
 import AnimatedWordSwap from "../animations/AnimatedWordSwap/AnimatedWordSwap";
 import DownloadButtons from "../components/DownloadButtons";
-import LogoGrid from "../components/LogoGrid";
+import UndrawTeam from "../assets/UndrawTeam.svg";
+import UndrawGolfer from "../assets/UndrawGolfer.svg";
 import GolfNowLogo from '../assets/logos/GolfNowLogo.svg';
+import Footer from "../components/Footer";
 
 const Hero = () => {
     return (
@@ -21,21 +23,115 @@ const Hero = () => {
     )
 }
 
-const LogoGrid = () => {
+const PartnersGrid = () => {
     return (
-        <div className="grid grid-cols-4 grid-rows-2 gap-12 w-full py-24 place-items-center">
-            <img src={GolfNowLogo} alt='GolfNow Logo' className='w-44 h-auto col-start-1 row-start-1'/>
-            <img src={GolfNowLogo} alt='GolfNow Logo' className='w-44 h-auto col-start-2 row-start-1'/>
-            <img src={GolfNowLogo} alt='GolfNow Logo' className='w-44 h-auto col-start-3 row-start-1'/>
-            <img src={GolfNowLogo} alt='GolfNow Logo' className='w-44 h-auto col-start-4 row-start-1'/>
-            <img src={GolfNowLogo} alt='GolfNow Logo' className='w-44 h-auto col-start-1 row-start-2'/>
-            <img src={GolfNowLogo} alt='GolfNow Logo' className='w-44 h-auto col-start-2 row-start-2'/>
-            <img src={GolfNowLogo} alt='GolfNow Logo' className='w-44 h-auto col-start-3 row-start-2'/>
-            <img src={GolfNowLogo} alt='GolfNow Logo' className='w-44 h-auto col-start-4 row-start-2'/>
+        <div id="partners-grid" className="grid grid-cols-4 grid-rows-2 gap-12 w-full py-24 place-items-center">
+            <img id="golf-now-logo" src={GolfNowLogo} alt='GolfNow Logo' className='w-44 h-auto col-start-1 row-start-1'/>
+            <img id="golf-now-logo" src={GolfNowLogo} alt='GolfNow Logo' className='w-44 h-auto col-start-2 row-start-1'/>
+            <img id="golf-now-logo" src={GolfNowLogo} alt='GolfNow Logo' className='w-44 h-auto col-start-3 row-start-1'/>
+            <img id="golf-now-logo" src={GolfNowLogo} alt='GolfNow Logo' className='w-44 h-auto col-start-4 row-start-1'/>
+            <img id="golf-now-logo" src={GolfNowLogo} alt='GolfNow Logo' className='w-44 h-auto col-start-1 row-start-2'/>
+            <img id="golf-now-logo" src={GolfNowLogo} alt='GolfNow Logo' className='w-44 h-auto col-start-2 row-start-2'/>
+            <img id="golf-now-logo" src={GolfNowLogo} alt='GolfNow Logo' className='w-44 h-auto col-start-3 row-start-2'/>
+            <img id="golf-now-logo" src={GolfNowLogo} alt='GolfNow Logo' className='w-44 h-auto col-start-4 row-start-2'/>
         </div>
     );
 }
 
+const ExpandYourNetwork = () => {
+    return (
+        <div id="expand-your-network" className="flex flex-row w-full items-start justify-center py-24 gap-x-32">
+            <div id="expand-your-network-text-container" className="flex flex-col w-2/3 gap-y-8">
+                <div className="flex flex-col gap-y-2">
+                    <h2>Connect With New Partners and Expand Your Golf Network</h2>
+                    <p>
+                        Our platform connects golfers like never before, allowing you to meet new partners and expand your golf network. 
+                        With a wide range of golfers from beginners to professionals, you'll have the opportunity to play with players of all skill levels.
+                    </p>
+                </div>
+                <div className="flex flex-row gap-x-2">
+                    <div className="flex flex-col gap-y-2">
+                        <h3>Improve game</h3>
+                        <p>Playing with different partners can help you improve your game by learning new strategies and techniques.</p>
+                    </div>
+                    <div className="flex flex-col gap-y-2">
+                        <h3>Enjoy more</h3>
+                        <p>With our platform, you'll have more opportunities to play golf and enjoy more rounds.</p>
+                    </div>
+                </div>
+            </div>
+            <div id="expand-your-network-image-container" className="flex w-1/3">
+                <img id="expand-your-network-image" src={UndrawTeam} alt="team" className="w-full h-auto"/>
+            </div>
+        </div>
+    );
+}
+
+const FindYourPartner = () => {
+    return (
+        <div id="find-your-partner" className="flex flex-col justify-center items-center text-center py-24 gap-y-16">
+            <div className="flex flex-col gap-y-2">
+                <h2>Find your golf partner and start playing</h2>
+                <p>Join our platform and discover a community of golfers ready to play. Sign up, find a golf partner, and start enjoying the game today.</p>
+            </div>
+            <div className="flex flex-row items-center justify-center gap-x-8 w-full">
+                <div className="flex flex-col items-center justify-center gap-y-2 w-1/3">
+                    <h3>Sign up and create your profile</h3>
+                    <p>Create your profile and let others know your golfing preferences and availability.</p>
+                </div>
+                <div className="flex flex-col items-center justify-center gap-y-2 w-1/3">
+                    <h3>Find golf partners near you</h3>
+                    <p>Browse through a list of golfers in your area and connect with potential partners.</p>
+                </div>
+                <div className="flex flex-col items-center justify-center gap-y-2 w-1/3">
+                    <h3>Schedule a game and enjoy golfing</h3>
+                    <p>Coordinate with your golf partner, choose a golf course, and have a great time playing golf together.</p>
+                </div>
+            </div>
+            <div>
+                <DownloadButtons/>
+            </div>
+        </div>
+    )
+}
+
+const DiscoverThePower = () => {
+    return (
+        <div id="discover-the-power" className="flex flex-row w-full items-start justify-center py-24 gap-x-32">
+            <div id="discover-the-power-text-container" className="flex flex-col w-2/3 gap-y-8">
+                <div className="flex flex-col gap-y-2">
+                    <h2>Discover the Power of Our Golf Partner Matching Platform</h2>
+                    <p>Join our community of golfers and experience the thrill of finding the perfect playing partners. With our platform, you'll never have to golf alone again.</p>
+                </div>
+                <div className="flex flex-row gap-x-2">
+                    <div className="flex flex-col gap-y-2">
+                        <h3>50% Increase</h3>
+                        <p>Users finding their ideal golf partners through our platform</p>
+                    </div>
+                    <div className="flex flex-col gap-y-2">
+                        <h3>50% Increase</h3>
+                        <p>Successful matches leading to memorable golf games</p>
+                    </div>
+                </div>
+            </div>
+            <div id="discover-the-power-image-container" className="flex w-1/3">
+                <img id="discover-the-power-image" src={UndrawGolfer} alt="golfer" className="w-full h-auto"/>
+            </div>
+        </div>
+    );
+}
+
+const Testimonials = () => {
+    return (
+        <div id="testimonials" className="flex flex-col justify-center items-center text-center py-24 gap-y-8">
+            <h3>★★★★★</h3>
+            <h3>I have had an amazing experience using this platform. It has helped me connect with other golfers and find partners to play with.</h3>
+            <div className="flex flex-row">
+                <p>John Doe — Google Reviews</p>
+            </div>
+        </div>
+    )
+}
 
 const LandingPage = () => {
     return (
@@ -43,7 +139,12 @@ const LandingPage = () => {
             <img src={LandingPageBackground} id="hero-background" alt="hero-background" className="w-screen h-auto absolute inset-0 inline-block -z-50"/>
             <Navbar/>
             <Hero/>
-            <LogoGrid/>
+            <PartnersGrid/>
+            <ExpandYourNetwork/>
+            <FindYourPartner/>
+            <DiscoverThePower/>
+            <Testimonials/>
+            <Footer/>
         </div>
     );
 }
