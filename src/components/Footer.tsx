80e31084cbc442e7662c449c2532e1efaa823fb8
Aidan Froggatt <aidanfr@live.ca>
2024-07-29 20:51:12 -0400

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,21 +6,21 @@ const Footer = () => {
         <div id="footer" className="flex flex-row justify-between w-full py-24 border-t border-dark">
             <div id="contact-info" className="flex flex-col justify-between">
                 <h4 className="text-text">SwingersGolf</h4>
-                <p id="copyright" className="hover:text-text text-sm font-medium">© 2021 SwingersGolf, Inc.</p>
+                <p id="copyright" className="hover:text-text text-md font-medium">© 2021 SwingersGolf, Inc.</p>
             </div>
             <div className="flex flex-row gap-x-8">
                 <div id="pages" className="flex flex-col justify-start items-start gap-y-2">
-                    <p className="text-sm font-semibold">Pages</p>
+                    <p className="text-md font-semibold">Pages</p>
                     <div className="flex flex-col justify-start items-start gap-y-1">
-                        <button className="hover:text-text text-sm font-medium" onClick={() => navigate("/products")}>Products</button>
-                        <button className="hover:text-text text-sm font-medium" onClick={() => navigate("/about")}>About</button>
+                        <button className="hover:text-text text-md font-medium" onClick={() => navigate("/products")}>Products</button>
+                        <button className="hover:text-text text-md font-medium" onClick={() => navigate("/about")}>About</button>
                     </div>                
                 </div>
                 <div id="resources" className="flex flex-col justify-start items-start gap-y-2">
-                    <p className="text-sm font-semibold">Resources</p>
+                    <p className="text-md font-semibold">Resources</p>
                     <div className="flex flex-col justify-start items-start gap-y-1">
-                        <button className="hover:text-text text-sm font-medium" onClick={() => navigate("/privacy-policy")}>Privacy & terms</button>
-                        <button className="hover:text-text text-sm font-medium" onClick={() => navigate("/privacy-policy")}>Contact</button>
+                        <button className="hover:text-text text-md font-medium" onClick={() => navigate("/privacy-policy")}>Privacy & terms</button>
+                        <button className="hover:text-text text-md font-medium" onClick={() => navigate("/privacy-policy")}>Contact</button>
                     </div>
                 </div>
             </div>
