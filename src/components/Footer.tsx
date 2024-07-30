64622e879e3b921d778e3f71b147a12da4d28916
Aidan Froggatt <aidanfr@live.ca>
2024-07-29 20:50:01 -0400

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,23 +4,25 @@ const Footer = () => {
     const navigate = useNavigate();
     return (
         <div id="footer" className="flex flex-row justify-between w-full py-24">
-            <div id="contact-info" className="flex flex-col justify-between w-1/4">
+            <div id="contact-info" className="flex flex-col justify-between">
                 <h4 className="text-text">SwingersGolf</h4>
-                <p id="copyright" className="text-sm">© 2021 SwingersGolf, Inc.</p>
+                <p id="copyright" className="hover:text-text text-sm font-medium">© 2021 SwingersGolf, Inc.</p>
             </div>
-            <div id="pages" className="flex flex-col justify-start items-start w-1/4 gap-y-2">
-                <p>Pages</p>
-                <button onClick={() => navigate("/products")}>Products</button>
-                <button onClick={() => navigate("/about")}>About</button>
-            </div>
-            <div id="contact" className="flex flex-col justify-start items-start w-1/4 gap-y-2">
-                <p>Contact</p>
-                <button onClick={() => navigate("/contact")}>Contact us</button>
-            </div>
-            <div id="resources" className="flex flex-col justify-start items-start w-1/4 gap-y-2">
-                <p>Resources</p>
-                <button onClick={() => navigate("/products")}>Privacy Policy</button>
-                <button onClick={() => navigate("/products")}>Terms of service</button>
+            <div className="flex flex-row gap-x-8">
+                <div id="pages" className="flex flex-col justify-start items-start gap-y-2">
+                    <p className="text-sm font-semibold">Pages</p>
+                    <div className="flex flex-col justify-start items-start gap-y-1">
+                        <button className="hover:text-text text-sm font-medium" onClick={() => navigate("/products")}>Products</button>
+                        <button className="hover:text-text text-sm font-medium" onClick={() => navigate("/about")}>About</button>
+                    </div>                
+                </div>
+                <div id="resources" className="flex flex-col justify-start items-start gap-y-2">
+                    <p className="text-sm font-semibold">Resources</p>
+                    <div className="flex flex-col justify-start items-start gap-y-1">
+                        <button className="hover:text-text text-sm font-medium" onClick={() => navigate("/privacy-policy")}>Privacy & terms</button>
+                        <button className="hover:text-text text-sm font-medium" onClick={() => navigate("/privacy-policy")}>Contact</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
