951efc7bb49642ffa5b19d3304197771d0ea71b8
Aidan Froggatt <aidanfr@live.ca>
2024-08-19 22:54:22 -0400

    restyled account page

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,9 +1,8 @@
 import Page from "@components/Page";
-import Navbar from "@components/Navbar";
 import { useAuth } from "@contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
 import TextButton from "@components/buttons/TextButton";
-import Footer from "@components/Footer";
+import { FiX } from "react-icons/fi";
 
 const Account = () => {
     const { account, signOut } = useAuth();
@@ -16,7 +15,17 @@ const Account = () => {
 
     return (
         <Page id="account">
-            <div id="account-content" className="flex flex-col justify-start items-start w-full min-h-screen">
+            <div className="irregular-background bg-dark-green" />
+            <div id="account-content" className="flex flex-col justify-start items-start w-full relative my-16">
+                <div className="flex flex-col items-center absolute top-0 right-0">
+                    <div
+                        className="flex justify-center items-center font-medium bg-dark w-12 h-12 rounded-full bg-opacity-30 text-sm border border-opacity-10 border-dark backdrop-blur cursor-pointer"
+                        onClick={() => navigate('/')}
+                    >
+                        <FiX className="text-primary w-9 h-9"/>
+                    </div>
+                    <p>Esc</p>
+                </div>
                 <h2>Account</h2>
                 <div>
                     <h3>Email</h3>
@@ -32,7 +41,6 @@ const Account = () => {
                     <TextButton onClick={handleSignOut} text="Sign Out" ariaLabel="Sign Out Button"/>
                 </div>
             </div>
-            <Footer/>
         </Page>
     );
 };
