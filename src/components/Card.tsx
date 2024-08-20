9e19467f8db81db19249b7c2b770c130f0ae7309
Aidan Froggatt <aidanfr@live.ca>
2024-08-20 01:07:40 -0400

    responsive user management forms

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@
 
 const Card = ({ children, id, className }: { children: React.ReactNode, id?: string, className?: string }) => {
     return (
-        <div id={id} className={`${className} flex flex-col bg-white shadow-card rounded-lg p-8 md:p-16 z-30`}>
+        <div id={id} className={`${className} flex flex-col bg-white shadow-card rounded-lg p-6 md:p-16 z-30`}>
             {children}
         </div>
     );
