82b1f0f3058b972d3aa0f2409b10f6b0906dd160
Aidan Froggatt <aidanfr@live.ca>
2024-08-19 22:32:15 -0400

    updated account menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,18 @@ import { useNavigate } from 'react-router-dom';
 import ChevronArrowButton from "@components/buttons/ChevronArrowButton";
 import LogoButton from '@components/buttons/LogoButton';
 import { useAuth } from '@contexts/AuthContext';
+import IconMenu from './IconMenu';
+import { FiUser, FiX } from 'react-icons/fi';
 
 const Navbar = () => {
     const navigate = useNavigate();
-    const { token, account } = useAuth();
+    const { token, account, signOut } = useAuth();
+
+    const accountMenuOptions = [
+        { name: 'Account', handleClick: () => navigate('/account') },
+        { name: 'Log Out', handleClick: () => signOut() }
+    ];
+
     return (
         <nav id="navbar" className="flex flex-row justify-between items-center h-20 w-full text-light font-semibold text-base z-50">
             <div id="pages" className="flex flex-row gap-x-12">
@@ -17,8 +25,8 @@ const Navbar = () => {
                 </button>
             </div>
                 { token ?
-                    <div id="account" onClick={() => navigate('/account')} className='flex items-center justify-center hover:cursor-pointer h-8 w-8 rounded-full bg-dark-green text-primary' aria-label='Account Icon'>
-                        {Array.from(account?.name ?? '')[0]}
+                    <div id="account"  className="flex flex-col">
+                        <IconMenu initialIcon={<FiUser className="w-9 h-9"/>} toggleIcon={<FiX className="w-9 h-9"/>} menuOptions={accountMenuOptions}/>
                     </div>
                     :
                     <div id="account" className="flex flex-row gap-x-4">
